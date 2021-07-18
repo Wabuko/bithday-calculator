f0a63b33b686f43b5014b846610869017a7ed2a2
wabuko <wabuko.akhanyinya@student.moringaschool.com>
2021-07-18 11:17:01 +0300

    add function akan day together with the variaables

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 function day (cc,yy,mm,dd){
     return (((cc/4)-2*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7
 }
+function akanDay () {
+    // UI variables
+var mm = parseInt(document.querySelector('#month').value);
+var dd = parseInt(document.querySelector('#date').value);
+var cc = parseInt(document.querySelector('#century').value);
+var yy = parseInt(document.querySelector('#year-digits').value);
+var gender = document.querySelector('#gender').value;
+var result=day (cc,yy,mm,dd);
+var males = new Array ("Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame");
+var females = new Array("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama");
